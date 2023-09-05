6cff4e37508ed62fc92f57e65cc6951cb9713020
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2023-09-05 17:03:05 +0300

    chore: init react-flow setup

---
+++ Frontend/src/flow.tsx
@@ -0,0 +1,42 @@
+import ReactFlow, { MiniMap, Controls, Background, BackgroundVariant, NodeTypes } from "reactflow";
+import { shallow } from "zustand/shallow";
+import useStore, { RFState } from "./state/store";
+
+import "reactflow/dist/style.css";
+
+const selector = (state: RFState) => ({
+  nodes: state.nodes,
+  edges: state.edges,
+  onNodesChange: state.onNodesChange,
+  onEdgesChange: state.onEdgesChange,
+  onConnect: state.onConnect,
+});
+
+const nodeTypes: NodeTypes = {
+
+};
+
+function Flow({ children }: any) {
+  const { nodes, edges, onNodesChange, onEdgesChange, onConnect } = useStore(selector, shallow);
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        nodeTypes={nodeTypes}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+        {children}
+      </ReactFlow>
+    </div>
+  );
+}
+
+export default Flow;
